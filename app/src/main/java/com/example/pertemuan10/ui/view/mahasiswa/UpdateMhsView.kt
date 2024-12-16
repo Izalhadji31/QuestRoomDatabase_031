a04b89613c10249e6d1ffd42b3074b743b523d52
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 22:52:09 +0700

    Isi body form untuk update data

--- app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,15 +1,26 @@
 package com.example.pertemuan10.ui.view.mahasiswa
 
+import InsertBodyMhs
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan10.ui.customwidget.TopAppBar
 import com.example.pertemuan10.ui.viewmodel.PenyediaViewModel
 import com.example.pertemuan10.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun UpdateMhsView(
@@ -34,4 +45,46 @@ fun UpdateMhsView(
             }
         }
     }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                judul = "Edit Mahasiswa",
+                showBackButton = true
+            )
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Isi body form untuk update data
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if (viewModel.validateFields()) {
+                            viewModel.updateData()
+                            delay(600)
+                            withContext(Dispatchers.Main) {
+                                onNavigate() // Navigasi di main thread
+                            }
+                        } else {
+                            // Tampilkan error jika validasi gagal
+                            snackbarHostState.showSnackbar("Field tidak valid!")
+                        }
+                    }
+                }
+            )
+        }
+    }
+
 }
