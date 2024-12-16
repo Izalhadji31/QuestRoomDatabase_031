66ef6921a2ca4a5317e5871c9adcca8e1da361e6
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:04:02 +0700

    Memperbaiki bug crud pada RepositoryMhs.kt

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -1,20 +1,14 @@
 package com.example.pertemuan10.repository
 
-import androidx.room.Delete
-import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
+
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
-    fun getAllMhs() : Flow<List<Mahasiswa>> //methode untuk memanggil fungsi untuk mendapatkan semua data
-
-    fun getMhs(nim: String) : Flow<Mahasiswa> //mengambil data mahasiswa berdasarkan Nim
-
-    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menghapus data mahasiswa
-
-    suspend fun updateMhs(mahasiswa: Mahasiswa) //memperbarui data mahasiswa di dalam database
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs (nim: String): Flow<Mahasiswa>
+    suspend fun deleteMhs(mahasiswa: Mahasiswa)
+    suspend fun updateMhs(mahasiswa: Mahasiswa)
 
 }
