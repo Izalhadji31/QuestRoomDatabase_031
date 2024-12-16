291f5afdcc27bea894c4e72899e4cb7da18288d5
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 13:48:36 +0700

    menambahkan fungsi CRUD pada RepositoryMhs

--- app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/RepositoryMhs.kt
@@ -1,7 +1,20 @@
 package com.example.pertemuan10.repository
 
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
+import java.util.concurrent.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs() : Flow<List<Mahasiswa>> //methode untuk memanggil fungsi untuk mendapatkan semua data
+
+    fun getMhs(nim: String) : Flow<Mahasiswa> //mengambil data mahasiswa berdasarkan Nim
+
+    suspend fun deleteMhs(mahasiswa: Mahasiswa) // menghapus data mahasiswa
+
+    suspend fun updateMhs(mahasiswa: Mahasiswa) //memperbarui data mahasiswa di dalam database
+
 }
