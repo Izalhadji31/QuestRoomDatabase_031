ed3dc66924f08580e81fe5a7e9c9516d30459103
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:06:46 +0700

    Memperbaiki bug crud pada MahasiswaDao.kt

--- app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10/data/dao/MahasiswaDao.kt
@@ -6,22 +6,22 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.example.pertemuan10.data.entity.Mahasiswa
-import java.util.concurrent.Flow
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface MahasiswaDao {
-    //fungsi get all data
-    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
-    fun getAllMahasiswa() : Flow<List<Mahasiswa>>
-    // get Mahasiswa
-    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")  //mengambil data mahasiswa berdasarkan NIM
-    fun getMahasiswa(nim: String) : Flow<Mahasiswa>
-    //Delete Mahasiswa
-    @Delete
-    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)  //menghapus data mahasiswa tertentu dari database
-    //Update Mahasiswa
-    @Update
-    suspend fun updateMahasiswa(mahasiswa: Mahasiswa) //memperbarui informasi mahasiswa yang sudah ada di database
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    @Query("SELECT * FROM mahasiswa ORDER BY nama ASC")
+    fun getAllMahasiswa(): Flow<List<Mahasiswa>>
+
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
+
+    @Delete
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+
+    @Update
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
