d56fedbde40f75874ba213dd47c4a291c36ce797
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 13:51:16 +0700

    memanggil fungsi CRUD

--- app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.pertemuan10.repository
 
 import com.example.pertemuan10.data.dao.MahasiswaDao
 import com.example.pertemuan10.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,5 +10,19 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
 
+    override fun getMhs(nim: String): Flow<Mahasiswa> {  //mengambil data mahasiswa berdasarkan nim
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) { //menghapus data mahasiswa
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) { //memperbarui data mahasiswa dalam database
+        mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
 }
