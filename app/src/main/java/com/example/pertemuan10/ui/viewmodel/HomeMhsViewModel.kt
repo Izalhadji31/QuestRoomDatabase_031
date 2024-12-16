27aa02a34e30b221ee902d59e6dd13034c0316c5
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:15:46 +0700

    Memastikan repository mengembalikan data non-null

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -2,7 +2,19 @@ package com.example.pertemuan10.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
-) : ViewModel()
+) : ViewModel(){
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull() // Pastikan repository mengembalikan data non-null
+        .map { listMahasiswa ->
+            HomeUiState(
+                listMhs = listMahasiswa.toList(),
+                isLoading = false
+            )
+        }
+}
