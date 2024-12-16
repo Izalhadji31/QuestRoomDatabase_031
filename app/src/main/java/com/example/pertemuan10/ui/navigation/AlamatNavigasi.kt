a4207b6ae1ff603fb3d4aa684b16388a484a301b
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:10:30 +0700

    Menambahkan object  DestinasiDetail : AlamatNavigasi

--- app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/pertemuan10/ui/navigation/AlamatNavigasi.kt
@@ -5,4 +5,9 @@ interface AlamatNavigasi {
     object  DestinasiHome : AlamatNavigasi {
         override val route = "home"
     }
+    object DestinasiDetail : AlamatNavigasi{
+        override val route = "detail"
+        const val NIM = "nim"
+        val routesWithArg = "$route/{$NIM}"
+    }
 }
