88b3d4096ffa2fcaf10af142792c2758d6e68353
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:14:27 +0700

    Menambahkan class HomeMhsViewModel

---
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan10.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan10.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel()
