3320e2b03ab92bae610453d255fc8694747f301d
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:57:33 +0700

    Menampilkan indikator loading

--- app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,11 @@
 package com.example.pertemuan10.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -13,12 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan10.ui.customwidget.TopAppBar
 import com.example.pertemuan10.ui.viewmodel.HomeMhsViewModel
+import com.example.pertemuan10.ui.viewmodel.HomeUiState
 import com.example.pertemuan10.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
 fun HomeMhsView(
@@ -60,5 +66,27 @@ fun HomeMhsView(
             coroutineScope = coroutineScope,
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    snackbarHostState: SnackbarHostState,
+    coroutineScope: CoroutineScope,
+    modifier: Modifier = Modifier
+) {
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
     }
 }
