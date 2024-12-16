c83e01264b234149fb85b39667168e4be43056eb
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 22:47:19 +0700

    menambahkan Observasi perubahan snackBarMessage

--- app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/UpdateMhsView.kt
+++ app/src/main/java/com/example/pertemuan10/ui/view/mahasiswa/UpdateMhsView.kt
@@ -1,8 +1,15 @@
 package com.example.pertemuan10.ui.view.mahasiswa
 
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import com.example.pertemuan10.ui.viewmodel.PenyediaViewModel
 import com.example.pertemuan10.ui.viewmodel.UpdateMhsViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun UpdateMhsView(
@@ -11,5 +18,20 @@ fun UpdateMhsView(
     modifier: Modifier = Modifier,
     viewModel: UpdateMhsViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val uiState = viewModel.updateUIState // Ambil UI state dari ViewModel
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    // Observasi perubahan snackBarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { massage ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(
+                    message = massage,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()  // Reset snackbar message
+            }
+        }
+    }
 }
