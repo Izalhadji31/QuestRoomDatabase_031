4249d26d2ed956cd4431d903795e74959c854b49
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:18:49 +0700

    menambahkan data class HomeUiState

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,12 +1,17 @@
 package com.example.pertemuan10.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pertemuan10.data.entity.Mahasiswa
 import com.example.pertemuan10.repository.RepositoryMhs
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class HomeMhsViewModel(
     private val repositoryMhs: RepositoryMhs
@@ -23,4 +28,25 @@ class HomeMhsViewModel(
             emit(HomeUiState(isLoading = true))
             delay(900) // Delay untuk simulasi loading
         }
+        .catch { exception ->
+            emit(
+                HomeUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = exception.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeUiState(isLoading = true)
+        )
 }
+
+data class HomeUiState(
+    val listMhs: List<Mahasiswa> = emptyList(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
