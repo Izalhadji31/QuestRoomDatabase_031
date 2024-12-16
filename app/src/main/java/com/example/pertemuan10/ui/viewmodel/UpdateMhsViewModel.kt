5f2544f28c868e681938bdf33503baa59930cd3d
Izalhadji31 <ryzalhadji@gmail.com>
2024-12-16 21:33:33 +0700

    menambahkan Deklarasi state UI

--- app/src/main/java/com/example/pertemuan10/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,10 +1,20 @@
 package com.example.pertemuan10.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10.repository.RepositoryMhs
+import com.example.pertemuan10.ui.navigation.AlamatNavigasi
 
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
-) : ViewModel()
+) : ViewModel(){
+    // Deklarasi state UI
+    var updateUIState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[AlamatNavigasi.DestinasiUpdate.NIM])
+}
